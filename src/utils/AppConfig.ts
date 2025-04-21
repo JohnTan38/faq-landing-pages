54202e2d9c9da513ea64914b83ac98c876cbb16a
John <psg6868@yahoo.com>
2025-04-21 11:19:43 +0800

    Add files via upload

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: 'Esker Invoice Automation',
+  title: 'Esker Invoice Automation',
+  description:
+    'Documentation for Esker Invoice Automation with React and Next.js',
+  locale: 'en',
+};
